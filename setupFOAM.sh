cc77e2bce95c3a5494707eade55e88d850590b74
Thijs Van_den_Brande <thijs.vandenbrande@bwk.kuleuven.be>
2013-07-10 15:19:33 +0200

    openFOAM installer added, user configuration not finished

---
+++ setupFOAM.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+# Simple setupFOAM.sh for configuring Ubuntu 12.04 for OPENFOAM. 
+
+# Install nvm: node-version manager
+# https://github.com/creationix/nvm
+sudo apt-get install -y git-core
+curl https://raw.github.com/creationix/nvm/master/install.sh | sh
+
+# Load nvm and install latest production node
+source $HOME/.nvm/nvm.sh
+nvm install v0.10.12
+nvm use v0.10.12
+
+# Install jshint to allow checking of JS code within emacs
+# http://jshint.com/
+npm install -g jshint
+
+# Install rlwrap to provide libreadline features with node
+# See: http://nodejs.org/api/repl.html#repl_repl
+sudo apt-get install -y rlwrap
+
+# Install emacs24
+# https://launchpad.net/~cassou/+archive/emacs
+sudo apt-add-repository -y ppa:cassou/emacs
+sudo apt-get update
+sudo apt-get install -y emacs24 emacs24-el emacs24-common-non-dfsg
+
+# git pull and install dotfiles as well
+cd $HOME
+if [ -d ./dotfiles/ ]; then
+    mv dotfiles dotfiles.old
+fi
+if [ -d .emacs.d/ ]; then
+    mv .emacs.d .emacs.d~
+fi
+git clone https://github.com/tvandenbrande/dotfiles.git
+ln -sb dotfiles/.screenrc .
+ln -sb dotfiles/.bash_profile .
+ln -sb dotfiles/.bashrc .
+ln -sb dotfiles/.bashrc_custom .
+ln -sf dotfiles/.emacs.d .
+
+# Install openFOAM
+VERS=$(lsb_release -cs)
+sudo sh -c "echo deb http://www.openfoam.org/download/ubuntu $VERS main > /etc/apt/sources.list.d/openfoam.list" 
+sudo apt-get update
+sudo apt-get install openfoam220
+sudo apt-get install paraviewopenfoam3120
+sudo apt-get install paraview
+# todo: user configuration, see http://www.openfoam.org/download/ubuntu.php
+
+
+
